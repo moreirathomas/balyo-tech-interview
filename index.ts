5230cdae0b962f7e988931139912f4edad61e484
Thomas Moreira <thomas.moreira@balyo.com>
2023-09-21 11:26:00 +0200

    feat: working solution

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-import { PowerUp, State } from "./state";
+type State = "SmallMario" | "SuperMario" | "FireMario";
+
+type PowerUp = "SuperMushroom" | "FireFlower";
 
 export class Player {
   state: State;
@@ -8,7 +10,41 @@ export class Player {
     this.lifes = 3;
   }
 
-  use(powerup: PowerUp): void {}
+  use(powerup: PowerUp): void {
+    type OneUp = 1;
+    type Transition = [State, PowerUp, State, OneUp?];
 
-  hit(): void {}
+    const transitions: Transition[] = [
+      ["SmallMario", "SuperMushroom", "SuperMario"],
+      ["SmallMario", "FireFlower", "FireMario"],
+      ["SuperMario", "FireFlower", "FireMario"],
+      ["SuperMario", "SuperMushroom", "SuperMario", 1],
+      ["FireMario", "SuperMushroom", "FireMario", 1],
+    ];
+
+    const transition = transitions.find(
+      ([state, pu]) => state === this.state && pu === powerup,
+    );
+    if (!transition) return; // noop, log error ?
+    const [, , newState, oneUp] = transition;
+    this.state = newState;
+    this.lifes += oneUp ?? 0;
+  }
+
+  hit(): void {
+    type OneDown = -1;
+    type Transition = [State, State, OneDown?];
+
+    const transitions: Transition[] = [
+      ["SmallMario", "SmallMario", -1],
+      ["SuperMario", "SmallMario"],
+      ["FireMario", "SmallMario"],
+    ];
+
+    const transition = transitions.find(([state]) => state === this.state);
+    if (!transition) return; // noop, log error ?
+    const [, newState, oneDown] = transition;
+    this.state = newState;
+    this.lifes += oneDown ?? 0;
+  }
 }
--- state.ts
+++
@@ -1,3 +0,0 @@
-export type State = "SmallMario" | "SuperMario" | "FireMario";
-
-export type PowerUp = "SuperMushroom" | "FireFlower";
